fa5a6753831e7f19e9e5a4313bd146b752838994
marstarek <tarekahmed1568@gmail.com>
2022-10-14 22:00:09 +0200

    s

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Modal = () => {
   return (
-    <div>Modal</div>
-  )
+<a href="#my-modal-2" className="btn">open modal</a>
+<!-- Put this part before </body> tag -->
+<div className="modal" id="my-modal-2">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="modal-action">
+     <a href="#" className="btn">Yay!</a>
+    </div>
+  </div>
+</div>  )
 }
 
 export default Modal
